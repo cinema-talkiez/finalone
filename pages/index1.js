37717af9383ba2300f85667d1f4e9efda89722ad
cinema-talkiez <cinema.talkiez.io@gmail.com>
2025-08-05 12:19:09 +0530

    Update index1.js

--- pages/index1.js
+++ pages/index1.js
@@ -203,7 +203,7 @@ const Index1 = () => {
           <div style={containerStyle}>
             <div
               style={cardStyle}
-              onClick={() => handleRedirect("https://one.ibomma.foo/")}
+              onClick={() => handleRedirect("https://www.google.com/")}
               onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.03)")}
               onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
             >
