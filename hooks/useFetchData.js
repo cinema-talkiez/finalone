41088153ae3a6c695606b1b8ebd4288e6ffe8da2
Madhan00000 <hni89873@gmail.com>
2025-10-29 20:28:01 +0530

    Add files via upload

---
+++ hooks/useFetchData.js
@@ -0,0 +1,41 @@
+// useFetchData.js
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function useFetchData(apiEndpoint) {
+  const [alldata, setAllData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [initialLoad, setInitialLoad] = useState(true);
+  const [allMovie, setAllMovie] = useState([]);
+
+  useEffect(() => {
+    if (initialLoad) {
+      setInitialLoad(false);
+      setLoading(false);
+      return;
+    }
+
+    setLoading(true);
+
+    const fetchAllData = async () => {
+      try {
+        const res = await axios.get(apiEndpoint);
+        const alldata = res.data;
+        setAllData(alldata);
+        setAllMovie(alldata);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching movie data:", error);
+        setLoading(false);
+      }
+    };
+
+    if (apiEndpoint) {
+      fetchAllData();
+    }
+  }, [initialLoad, apiEndpoint]);
+
+  return { alldata, allMovie, loading };
+}
+
+export default useFetchData;
